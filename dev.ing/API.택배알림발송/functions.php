b6c5404abb8ad3cbe04d0c21a8878810bbe75819
banana0117 <yjk@oliveinc.co.kr>
2021-03-19 10:59:16 +0900

    wp-cron schedule moduler

--- dev.ing/API.택배알림발송/functions.php
+++ dev.ing/API.택배알림발송/functions.php
@@ -1,5 +1,25 @@
 <?php
 
-add_action('test_check','test_db_story');
+add_filter( 'cron_schedules', 'isa_add_every_three_minutes' ); // 크론에 스케줄 추가
+function isa_add_every_three_minutes( $schedules ) { // 스케줄생성
+    $schedules['every_three_minutes'] = array(
+            'interval'  => 180, //초
+            'display'   => __( 'Every 3 Minutes', 'textdomain' ) // 표시될문구
+    );
+    return $schedules;
+}
 
-wp_schedule_event(time(), 'hourly', 'test_check');
+if ( ! wp_next_scheduled( 'isa_add_every_three_minutes' ) ) { // 해당이름으로된 스케줄이 없을경우
+    wp_schedule_event( time(), 'every_three_minutes', 'isa_add_every_three_minutes' ); // 해당 구문 실행, 조건은 위의 스케줄생성
+}
+
+add_action( 'isa_add_every_three_minutes', 'every_three_minutes_event_func' ); // 해당 훅에서 실행될 함수
+
+function every_three_minutes_event_func() { // 스케줄마다 실행될 함수
+    $mysqli = new mysqli('localhost', 'olivejnainc', 'Goyo5713**', 'olivejnainc');
+    $testz = date("h:i:s");
+    $mysqli_query = "INSERT INTO `userTest`(`userid`) VALUES ('$testz')";
+    mysqli_query($mysqli,$mysqli_query);
+    mysqli_close($mysqli);
+    // do something
+}
